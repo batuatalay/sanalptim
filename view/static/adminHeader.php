15960433962478be33d5529cbdf0ee3d0bd91d02
batuhan-atalay <batuhanatalay@jotform.com>
2023-03-22 22:44:56 +0300

    v2.0 :: litle fix

--- view/static/adminHeader.php
+++ view/static/adminHeader.php
@@ -100,69 +100,101 @@
             <!-- Menu -->
             <div class="menu">
                 <ul class="list">
-                    <li class="active">
+                    <li>
                         <a href="index.html">
                             <i class="material-icons">home</i>
-                            <span>Home</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="pages/typography.html">
-                            <i class="material-icons">text_fields</i>
-                            <span>Typography</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="pages/helper-classes.html">
-                            <i class="material-icons">layers</i>
-                            <span>Helper Classes</span>
+                            <span>Anasayfa</span>
                         </a>
                     </li>
                     <li>
                         <a class="menu-toggle">
-                            <i class="material-icons">widgets</i>
-                            <span>Widgets</span>
+                            <span>Branşlar</span>
                         </a>
                         <ul class="ml-menu">
                             <li>
-                                <a class="menu-toggle">
-                                    <span>Cards</span>
+                                <a class=" waves-effect waves-block">
+                                    <span>Branş Ekle</span>
                                 </a>
-                                <ul class="ml-menu">
-                                    <li>
-                                        <a href="pages/widgets/cards/basic.html">Basic</a>
-                                    </li>
-                                    <li>
-                                        <a href="pages/widgets/cards/colored.html">Colored</a>
-                                    </li>
-                                    <li>
-                                        <a href="pages/widgets/cards/no-header.html">No Header</a>
-                                    </li>
-                                </ul>
                             </li>
                             <li>
-                                <a class="menu-toggle">
-                                    <span>Infobox</span>
+                                <a class=" waves-effect waves-block">
+                                    <span>Branşlar</span>
                                 </a>
-                                <ul class="ml-menu">
-                                    <li>
-                                        <a href="pages/widgets/infobox/infobox-1.html">Infobox-1</a>
-                                    </li>
-                                    <li>
-                                        <a href="pages/widgets/infobox/infobox-2.html">Infobox-2</a>
-                                    </li>
-                                    <li>
-                                        <a href="pages/widgets/infobox/infobox-3.html">Infobox-3</a>
-                                    </li>
-                                    <li>
-                                        <a href="pages/widgets/infobox/infobox-4.html">Infobox-4</a>
-                                    </li>
-                                    <li>
-                                        <a href="pages/widgets/infobox/infobox-5.html">Infobox-5</a>
-                                    </li>
-                                </ul>
                             </li>
                         </ul>
+                    </li>
+                    <li>
+                        <a class="menu-toggle">
+                            <span>Hocalar</span>
+                        </a>
+                        <ul class="ml-menu">
+                            <li>
+                                <a class=" waves-effect waves-block">
+                                    <span>Hoca Ekle</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a class=" waves-effect waves-block">
+                                    <span>Hocalar</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a class="menu-toggle">
+                            <span>Hareketler</span>
+                        </a>
+                        <ul class="ml-menu">
+                            <li>
+                                <a class=" waves-effect waves-block">
+                                    <span>Hareket Ekle</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a class=" waves-effect waves-block">
+                                    <span>Hareketler</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a class="menu-toggle">
+                            <span>Programlar</span>
+                        </a>
+                        <ul class="ml-menu">
+                            <li>
+                                <a class=" waves-effect waves-block">
+                                    <span>Program Ekle</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a class=" waves-effect waves-block">
+                                    <span>Programlar</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a class="menu-toggle">
+                            <span>Yazılar</span>
+                        </a>
+                        <ul class="ml-menu">
+                            <li>
+                                <a class=" waves-effect waves-block">
+                                    <span>Yazı Ekle</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a class=" waves-effect waves-block">
+                                    <span>Yazılar</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="pages/typography.html">
+                            <span>Site Ayarları</span>
+                        </a>
                     </li>
                 </ul>
             </div>
